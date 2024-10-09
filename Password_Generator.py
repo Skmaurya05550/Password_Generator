09c0e256fa613980c808db69ca2a2f8306b21b84
Shubham Maurya <157043197+Skmaurya05550@users.noreply.github.com>
2024-10-09 23:50:14 +0530

    Create Password_Generator.py

---
+++ Password_Generator.py
@@ -0,0 +1,12 @@
+# Password Generator by the python code .
+
+import random 
+chars = "abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+"
+lenght = int (input(" Enter  password lenght : "))
+password = " "
+for a in range (lenght ):
+  password +=random.choice(chars)
+print(password)
+
+
+
